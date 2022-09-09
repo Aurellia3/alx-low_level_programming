2571944c039252c07b356b155b38a1bff30a2cc1
Wendy Aurellia <wendyaurellia@gmail.com>
2022-09-09 13:16:49 -0700

    unix simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <unistd.h>
-
 /**
  * main - print quote text
- *
  * Description: prints Doras's quote
- *
+ * ssize_t write(int fd, const void *buf, size_t count);
  * Return: Always 1 (Success)
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
-	Return (1);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		write(1, quo, 59);
+			return (1);
 }
 
