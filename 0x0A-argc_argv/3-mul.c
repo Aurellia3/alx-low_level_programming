231210dd090daa33f9ad7d802bbf0c97e2028a8d
Wendy Aurellia <wendyaurellia@gmail.com>
2022-10-02 15:37:45 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,16 @@
 
 int main(int argc, char *argv[])
 {
-	int num_1, num_2, mul;
+	int m;
 
 	if (arg != 3)
+	{
 		printf("Error\n");
+		return (1);
+	}
 	else
 	{
-		num_1 = atoi(argv[1]);
-		num_2 = atoi(argv[2]);
-		mul = num_1 * num_2;
+		m = (atoi(argv[1]) * atoi(argv[2]));
 		printf("%d\n", mul);
 	}
 	return (0);
