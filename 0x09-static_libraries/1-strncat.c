ad63e8bd2da98af1dc66cc0a3b46587450b35cd0
Wendy Aurellia <wendyaurellia@gmail.com>
2022-10-03 09:57:32 -0700

    static libraries

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings but add input on number of bytes
+ * @dest: string to be appended upon
+ * @src: string to be completed at end of dust
+ * @n: integer parameter to compare index to
+ *
+ * Return: Returns new concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int c, i;
+
+	c = 0;
+
+	/*find sizeof dest array*/
+	while (dest[c])
+		c++;
+
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	 */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	/*null terminate dest*/
+	dest[c + i] = '\0';
+
+	return (dest);
+}
