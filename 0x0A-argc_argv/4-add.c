8e8b9aa8c8dcb94c8783ed40a3f3d223464ad451
Wendy Aurellia <wendyaurellia@gmail.com>
2022-10-03 08:49:56 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,29 +4,62 @@
 #include <string.h>
 
 /**
- * main - adds numbers
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
- * Return: 0 if no errors, else 1
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (success)
+ */
+
+int check_num(char *str)
+{
+/*Declaring variables*/
+unsigned int count;
+
+count = 0;
+while (count < strlen(str)) /*count string*/
+
+{
+if (!isdigit(str[count])) /*check if str there are digit*/
+{
+return (0);
+}
+
+count++;
+}
+return (1);
+}
+
+/**
+ * main - print the name of the program
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ * Return: always 0 (success)
  */
 
 int main(int argc, char *argv[])
 {
-	int a = 0, i, j;
+/*Declaring variables*/
+int count;
+int str_to_int;
+int sum = 0;
 
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j]; j++)
-		{
-			if (isdigit(argv[i][j]) == 0)
-			{
-				puts("Error");
-				return (1);
-			}
-		}
-	}
-	for (i = 1; i < argc; i++)
-	{
-		a += atoi(argv[i]);
-	}
+count = 1;
+while (count < argc) /*Goes through the whole array*/
+{
+if (check_num(argv[count]))
+{
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+sum += str_to_int;
+}
+/*condition if one of the number contains symbols that are not digits*/
+else
+{
+printf("Error\n");
+return (1);
+}
+count++;
+}
+printf("%d\n", sum); /*print sum*/
+return (0);
 }
